100b9563b49ba0e20a1e76cc13629a83cf838fc5
Alejandro A <alejandroaguilardev>
2023-07-19 12:33:02 +0200

    port

--- src/config/joi.validation.ts
+++ src/config/joi.validation.ts
@@ -2,6 +2,6 @@ import * as Joi from 'joi';
 
 export const JoiValidationSchema = Joi.object({
   MONGODB: Joi.required(),
-  PORT: Joi.number().default(3005),
+  PORT: Joi.number(),
   DEFAULT_LIMIT: Joi.number().default(6),
 });
